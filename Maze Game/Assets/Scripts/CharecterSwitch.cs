6f7ca546d23ba9b8b5c144a7a00fbae01fea6df8
deptster <deepantripathi0000@gmail.com>
2021-03-26 22:43:46 +0530

    Banana Alpha 2

---
+++ Maze Game/Assets/Scripts/CharecterSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharecterSwitch : MonoBehaviour
+{	public GameObject Char1;
+    public GameObject Char2;
+    //public GameObject Canvas1;
+    //public GameObject Canvas2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            Char1.SetActive(true);
+            Char2.SetActive(false);
+            //Canvas1.SetActive(true);
+            //Canvas2.SetActive(false);
+        }
+        if(Input.GetKey(KeyCode.O))
+        {
+        	Char1.SetActive(false);
+            Char2.SetActive(true);
+            //Canvas1.SetActive(false);
+            //Canvas2.SetActive(true);
+        }
+
+    }
+}
---
+++ Maze Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject characterSwitch;
+
+    public AudioMixer audioMixer;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        characterSwitch.SetActive(true);
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        characterSwitch.SetActive(false);
+        GameIsPaused = true;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Maze 1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
